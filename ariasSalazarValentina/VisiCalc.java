0151d669b43bbdf7111f255e226962a31c8e3055
Valentina Arias Salazar <114306799+valentinarias@users.noreply.github.com>
2023-07-03 10:29:47 +0200

    Update VisiCalc.java
    Se agrego un indicador

--- ariasSalazarValentina/VisiCalc.java
+++ ariasSalazarValentina/VisiCalc.java
@@ -5,40 +5,46 @@ public class VisiCalc {
         Scanner scanner = new Scanner(System.in);
 
         String[][] tabla = new String[16][11];
-        String[] comados = { "w", "A", "S", "D" };
+        String[] comandos = { "W", "A", "S", "D" };
 
         int currentRow = 1;
         int currentCol = 1;
 
         tabla[0][0] = "+--------------------------------------------+\n";
-        tabla[0][1] = "|   | A | B | C | D | E | F | G | H | I |  J |\n";
+        tabla[0][1] = "|   | A | B | C | D | E | F | G | H | I | J |\n";
         tabla[0][2] = "|---+---+---+---+---+---+---+---+---+---+---+|\n";
 
         for (int fila = 1; fila <= 15; fila++) {
             tabla[fila][0] = "| " + fila + "|   ";
-            if(fila >= 10){
+            if (fila >= 10) {
                 tabla[fila][0] = "|" + fila + "|   ";
             }
         }
 
         for (int fila = 1; fila <= 15; fila++) {
             for (int columna = 1; columna <= 10; columna++) {
-                tabla[fila][columna] = "  | ";
+                tabla[fila][columna] = "|";
             }
         }
 
-        // Imprimir la tabla
+        // Imprimir la tabla con indicador de posición
         for (int fila = 0; fila <= 15; fila++) {
             for (int columna = 0; columna <= 10; columna++) {
                 if (tabla[fila][columna] == null) {
                     tabla[fila][columna] = "     ";
                 }
-                System.out.print(tabla[fila][columna]);
+                if (fila == currentRow && columna == currentCol) {
+                    System.out.print(" <" + tabla[fila][columna] + ">");
+                } else {
+                    System.out.print("  " + tabla[fila][columna] + " ");
+                }
             }
             System.out.println();
         }
+
         while (true) {
             System.out.print("Ingrese un comando (W/A/S/D/F): ");
+            System.out.println(CMDs:[W]Arriba,[S]Abajo,[A]izquierda,);
             String comando = scanner.next();
 
             if (comando.equals("W")) {
@@ -59,11 +65,27 @@ public class VisiCalc {
                 }
             } else {
                 if (comando.equals("F")) {
-                    System.out.println("the program end");
+                    System.out.println("El programa ha finalizado.");
+                    break;
                 } else {
                     System.out.println("Comando inválido. Intente nuevamente.");
                     continue;
                 }
+            }
+            
+            // Imprimir la tabla actualizada con indicador de posición
+            for (int fila = 0; fila <= 15; fila++) {
+                for (int columna = 0; columna <= 10; columna++) {
+                    if (tabla[fila][columna] == null) {
+                        tabla[fila][columna] = "     ";
+                    }
+                    if (fila == currentRow && columna == currentCol) {
+                        System.out.print(" <" + tabla[fila][columna] + ">");
+                    } else {
+                        System.out.print("  " + tabla[fila][columna] + " ");
+                    }
+                }
+                System.out.println();
             }
         }
     }
