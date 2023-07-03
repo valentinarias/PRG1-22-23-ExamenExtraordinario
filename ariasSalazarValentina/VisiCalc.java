c00ca8371908a2e499c11b63ca8526b949beeaf5
Valentina Arias Salazar <114306799+valentinarias@users.noreply.github.com>
2023-07-03 09:29:56 +0200

    VisiCalc.java
    Se agrego el tablero

---
+++ ariasSalazarValentina/VisiCalc.java
@@ -0,0 +1,21 @@
+public class VisiCalc{
+
+ public stattic void main (String[] args){
+
+String[][] table = new String[16][11]:
+
+ table[0][0]= "+--------------------------------------------+\n";
+ table[0][1]= "|   | A | B | C | D | E | F | G | H | I | J  |\n";
+ table[0][2]= "|---+---+---+---+---+---+---+---+---+---+---+|\n";
+
+  for(int fila =1;fila<=15;filaa++)
+   
+
+
+
+
+
+
+   
+ }
+}
